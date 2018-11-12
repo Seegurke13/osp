050f568b4d5efdc59d12f473e00f694d7a53d854
Gitea <gitea@fake.local>
2018-11-12 14:58:29 +0100

    init

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Seegurke
+ * Date: 12.11.2018
+ * Time: 14:46
+ */
+
+namespace App\Controller;
+
+
+class IndexController
+{
+    public function __construct()
+    {
+    }
+}
